25abb618748f40eb492fce842d47e3439894945e
Nahue <nlcostamagna@gmail.com>
2025-08-13 18:15:32 -0300

    get endpoint

--- go/grpcPassit/go.mod
+++ go/grpcPassit/go.mod
@@ -1,6 +1,6 @@
 module github.com/ncostamagna/passit-proto/go/grpcPassit
 
-go 1.24.5
+go 1.25.0
 
 require (
 	google.golang.org/grpc v1.74.2
--- go/grpcPassit/passit_grpc.pb.go
+++ go/grpcPassit/passit_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type PassitClient interface {
 	CreateSecret(ctx context.Context, in *CreateSecretRequest, opts ...grpc.CallOption) (*CreateSecretResponse, error)
+	GetSecret(ctx context.Context, in *GetSecretRequest, opts ...grpc.CallOption) (*GetSecretResponse, error)
 }
 
 type passitClient struct {
@@ -42,11 +43,21 @@ func (c *passitClient) CreateSecret(ctx context.Context, in *CreateSecretRequest
 	return out, nil
 }
 
+func (c *passitClient) GetSecret(ctx context.Context, in *GetSecretRequest, opts ...grpc.CallOption) (*GetSecretResponse, error) {
+	out := new(GetSecretResponse)
+	err := c.cc.Invoke(ctx, "/grpcPassit.Passit/GetSecret", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // PassitServer is the server API for Passit service.
 // All implementations must embed UnimplementedPassitServer
 // for forward compatibility
 type PassitServer interface {
 	CreateSecret(context.Context, *CreateSecretRequest) (*CreateSecretResponse, error)
+	GetSecret(context.Context, *GetSecretRequest) (*GetSecretResponse, error)
 	mustEmbedUnimplementedPassitServer()
 }
 
@@ -56,6 +67,9 @@ type UnimplementedPassitServer struct {
 
 func (UnimplementedPassitServer) CreateSecret(context.Context, *CreateSecretRequest) (*CreateSecretResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateSecret not implemented")
+}
+func (UnimplementedPassitServer) GetSecret(context.Context, *GetSecretRequest) (*GetSecretResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetSecret not implemented")
 }
 func (UnimplementedPassitServer) mustEmbedUnimplementedPassitServer() {}
 
@@ -88,6 +102,24 @@ func _Passit_CreateSecret_Handler(srv interface{}, ctx context.Context, dec func
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Passit_GetSecret_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetSecretRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PassitServer).GetSecret(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/grpcPassit.Passit/GetSecret",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PassitServer).GetSecret(ctx, req.(*GetSecretRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Passit_ServiceDesc is the grpc.ServiceDesc for Passit service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -98,6 +130,10 @@ var Passit_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "CreateSecret",
 			Handler:    _Passit_CreateSecret_Handler,
+		},
+		{
+			MethodName: "GetSecret",
+			Handler:    _Passit_GetSecret_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
